d4ae3010222d7425fdb2f40c9d66684c561b7d53
unknown <jackchen010@yahoo.com>
2018-03-05 16:58:18 -0800

    Edited some styling for timings.m

--- timings.m
+++ timings.m
@@ -25,7 +25,7 @@ function [N_S_ped_on, E_W_ped_on] = timings(desired_time, ljHandle, active_ped_l
         % If the East or West pedestrian button was not pressed before, set
         % the variable so that it has been pressed.      
         if (~E_W_ped_on)
-            [Error EWstate] = ljud_eGet(ljHandle, LJ_ioGET_DIGITAL_BIT,0,0,0);
+            [Error, EWstate] = ljud_eGet(ljHandle, LJ_ioGET_DIGITAL_BIT,0,0,0);
             Error_Message(Error)
             if (EWstate == 0)
                 E_W_ped_on = true;
@@ -35,7 +35,7 @@ function [N_S_ped_on, E_W_ped_on] = timings(desired_time, ljHandle, active_ped_l
         % If the North or South pedestrian button was not pressed before,
         % set the variable so that it has been pressed.
         if (~N_S_ped_on)
-            [Error NSstate] = ljud_eGet(ljHandle, LJ_ioGET_DIGITAL_BIT,1, 0, 0);
+            [Error, NSstate] = ljud_eGet(ljHandle, LJ_ioGET_DIGITAL_BIT,1, 0, 0);
             Error_Message(Error)
             if (NSstate == 0)
                 N_S_ped_on = true;
